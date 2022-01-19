96ed38163e0344598d5943b49ad627b9d89ba034
Gavin Jackson <gav_jackson@yahoo.com>
2022-01-19 08:56:37 +0000

    initial commit from WIP

---
+++ server.js
@@ -0,0 +1,8 @@
+const connect = require('connect');
+const serveStatic = require('serve-static');
+const open = require('open');
+
+connect().use(serveStatic(__dirname + "/dist")).listen(1111, async function(){
+    console.log('Server running on port: 1111...');
+    await open('http://localhost:1111/index.html');
+});
