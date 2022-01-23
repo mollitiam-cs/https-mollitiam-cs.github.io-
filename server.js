4715913e404f1707178607a6d966d33be9e44c26
Gavin Jackson <gav_jackson@yahoo.com>
2022-01-23 13:48:43 +0000

    moving icons

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const connect = require('connect');
 const serveStatic = require('serve-static');
 const open = require('open');
 
-connect().use(serveStatic(__dirname + "/dist")).listen(1111, async function(){
+connect().use(serveStatic(__dirname + "/docs")).listen(1111, async function(){
     console.log('Server running on port: 1111...');
     await open('http://localhost:1111/index.html');
 });
